00e45fc05d13dc5f737a75e04065c6ccbba4f82e
[JaiBalaji Raj] <[balaravanan81726@gmail.com]>
2021-11-29 17:06:26 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
 import './App.css';
+import ReactPlayer from 'react-player'
 
 function App() {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '4cf55dfa9a0ab63a939fffb8ae325498',
+    lat: '1.2921',
+    lon: '36.8219',
+    lang: 'en',
+    unit: 'metric', 
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ReactWeather
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang='en'
+        locationLabel='Nairobi'
+        unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+        showForecast
+      />
+      <h3>React Player</h3>
+      <ReactPlayer url='https://www.youtube.com/watch?v=pflXnUNMsNk' />
+      <div>
+      <h3>Video.js</h3>
+      <video
+    id="my-player"
+    class="video-js"
+    controls
+    preload="auto"
+    poster="//vjs.zencdn.net/v/oceans.png"
+    data-setup='{}'>
+  <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4"></source>
+  <source src="//vjs.zencdn.net/v/oceans.webm" type="video/webm"></source>
+  <source src="//vjs.zencdn.net/v/oceans.ogv" type="video/ogg"></source>
+  <p class="vjs-no-js">
+    To view this video please enable JavaScript, and consider upgrading to a
+    web browser that
+    <a href="https://videojs.com/html5-video-support/" target="_blank">
+      supports HTML5 video
+    </a>
+  </p>
+</video>
+</div>
     </div>
   );
 }
